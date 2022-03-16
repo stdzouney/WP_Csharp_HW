504720effc8c725794286c45b69b6c68a1824859
정석전 <82124587+zourney97@users.noreply.github.com>
2022-03-16 22:35:45 +0900

    Update WPHW_0311_01.cs

--- WPHW_0311_01.cs
+++ WPHW_0311_01.cs
@@ -25,6 +25,7 @@ namespace ConsoleApp1
                     Console.WriteLine("짝수를 입력해주세요\n");
             }
 
+            //홀짝 리스트 구분
             List<int> odd = new List<int>();
             List<int> even = new List<int>();
 
@@ -38,8 +39,8 @@ namespace ConsoleApp1
                 Inputnum = Console.ReadLine();
                 Inum = Convert.ToInt32(Inputnum);
 
-                
-
+               
+                //리스트에 추가
                 if (Inum % 2 == 1)
                     odd.Add(Inum);
                 else
@@ -59,7 +60,7 @@ namespace ConsoleApp1
                             Console.Write(even[j] + " ");
                         }
                     }
-                    else
+                    else //예외처리
                     {
                         i = -1;
                         Console.WriteLine("홀수의 개수{0} 와 짝수의 개수{1} 가 같지않습니다. 처음부터 다시 입력해주세요\n",(odd.Count),(even.Count));
