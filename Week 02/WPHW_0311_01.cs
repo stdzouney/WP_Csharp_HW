d98d019cdabbc55372751e1b7c9e5b56bae1cf65
정석전 <82124587+stdzouney@users.noreply.github.com>
2022-03-17 18:11:34 +0900

    Add files via upload

---
+++ Week 02/WPHW_0311_01.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class WPHW_0311_01
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("입력받은 수를 정렬해주는 프로그램입니다 (홀짝 수가 맞게 입력해주세요). \n");
+            bool c = true;
+            string InputListsize; // 리스트의 크기
+            int Listsize = 0;
+
+            while (c)
+            {
+                Console.WriteLine("입력할 숫자의 갯수는? ");
+
+                InputListsize = Console.ReadLine();
+                Listsize = Convert.ToInt32(InputListsize); // 자료형변경
+                if (Listsize % 2 == 0)
+                    break;
+                else
+                    Console.WriteLine("짝수를 입력해주세요\n");
+            }
+            //홀짝 리스트 구분
+            List<int> odd = new List<int>();
+            List<int> even = new List<int>();
+
+            for (int i = 0; i<Listsize;i++ ) // 입력받은 리스트의 크기만큼 반복
+            {
+
+
+                
+                string Inputnum; 
+                int Inum = 0;
+                Console.WriteLine("숫자를 입력하세요 ");
+                Inputnum = Console.ReadLine();
+                Inum = Convert.ToInt32(Inputnum);
+
+
+                //리스트에 추가
+                if (Inum % 2 == 1)
+                    odd.Add(Inum);
+                else
+                    even.Add(Inum);
+
+                odd.Sort();
+                even.Sort();
+                even.Reverse();
+
+                if(i==(Listsize-1))
+                {
+                    if (odd.Count == even.Count)
+                    {
+                        for (int j = 0; j < odd.Count; j++)
+                        {
+                            Console.Write(odd[j] + " ");
+                            Console.Write(even[j] + " ");
+                        }
+                    }
+                    else //예외처리
+                    {
+                        i = -1;
+                        Console.WriteLine("홀수의 개수{0} 와 짝수의 개수{1} 가 같지않습니다. 처음부터 다시 입력해주세요\n",(odd.Count),(even.Count));
+                        odd.Clear();
+                        even.Clear();
+                    }
+                }
+                
+            }
+            
+
+            
+
+
+           
+            }
+            
+            
+
+           
+        }
+    }
+
---
+++ Week 02/WPHW_0311_02.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputNum;
+            int Inum = 0;
+            Console.WriteLine("1~1000까지 x의 갯수를 세는 프로그램입니다 x를 입력하세요 (1~9까지) \n");
+            InputNum = Console.ReadLine();
+            Inum = Int32.Parse(InputNum);
+            int CNT_NUM = 0;
+            for (int i = 1; i < 1001; i++)
+            {
+                if (i % 10 == Inum)
+                    CNT_NUM++;
+                if (i / 100 == Inum)
+                    CNT_NUM++;
+                if ((i / 10) / 10 == Inum)
+                    CNT_NUM++;
+                if (i / 1000 == Inum)
+                    CNT_NUM++;
+
+            }
+
+            Console.WriteLine("1~1000에서 {0}은 {1}개 있습니다", Inum, CNT_NUM);
+        }
+    }
+
+
+
+        }
+
+
+
