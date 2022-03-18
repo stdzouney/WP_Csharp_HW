ff435b4495eb8786b29ea2c14427526236194d9f
정석전 <82124587+stdzouney@users.noreply.github.com>
2022-03-18 21:43:27 +0900

    Add files via upload

---
+++ Week 03/WPHW_0318_01v2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WP_week2
+{
+    class WPHW_0318_01
+    {
+        static int SumWithoutNum (string[] number)
+        {
+            int a = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                a += Int32.Parse(number[i]);
+            }
+            return (45-a);
+        }
+        static void Main(string[] args)
+        {
+            string InputNum;
+            int Num1 = 0;
+            Console.WriteLine("입력받은 배열에서 없는숫자의 합을 구해주는 프로그램입니다.");
+            Console.WriteLine("콤마(,)기준으로 입력해주세요 (범위0~9)");
+            InputNum = Console.ReadLine();
+
+            //Num1 = Int32.Parse(InputNum);
+
+            /*int[] arrNum1 = new int[] { 1, 2, 3, 4, 5, 6 };
+            int[] arrNum2 = new int[] { 0,2,4,6,8 };
+            int[] arrNum3 = new int[] { 1,3,5,7,9 };
+            int[] arrNum4 = new int[] {1,2 };
+            
+            
+            Console.WriteLine(SumWithoutNum(arrNum1));
+            Console.WriteLine(SumWithoutNum(arrNum2));
+            Console.WriteLine(SumWithoutNum(arrNum3));
+            Console.WriteLine(SumWithoutNum(arrNum4));*/
+
+            string[] arrNum5 = InputNum.Split(',');
+            Console.WriteLine(SumWithoutNum(arrNum5));
+        }
+    }
+}
---
+++ Week 03/WPHW_0318_02v2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WP_week2
+{
+    class WPHW_0318_02
+    {
+        static string MiddleAlpabet(string s)
+        {
+
+            string ab = null;
+
+            if (s.Length % 2 == 0) //문자열이 길이가 짝수
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (i == s.Length / 2)
+                    {
+                        ab += s[i - 1];
+                    }
+                    else if (i == s.Length / 2 + 1)
+                    {
+                        ab += s[i - 1];
+                    }
+                }
+            }
+            else //문자열이 길이가 홀수
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (i == s.Length / 2 + 1)
+                    {
+                        ab += s[i - 1];
+                    }
+                }
+            }
+            return ab;
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello,world!" + " \"{0}\"", MiddleAlpabet("Hello,world!"));
+            Console.WriteLine("Software" + " \"{0}\"", MiddleAlpabet("Software"));
+            Console.WriteLine("ComputerScience" + " \"{0}\"", MiddleAlpabet("ComputerScience"));
+
+            string InputString;
+            Console.WriteLine("입력하신 문자열의 중간값을 구하는 프로그램입니다. 문자열을 입력하세요");
+            InputString = Console.ReadLine();
+            Console.WriteLine(MiddleAlpabet(InputString));
+
+
+        }
+    }
+}
